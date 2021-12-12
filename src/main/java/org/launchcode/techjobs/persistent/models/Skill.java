e4440ea4080e88790fbabb55c0091d01ee1f7ab8
Evan <evan@mckenty.us>
2021-12-12 18:12:41 -0500

    passes all tests but i dont trust it until i get a green checkmark, im being very stream of consciousness abt these posts because ive been staring at intellij for eight hours now

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,34 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank(message = "Field must not be blank")
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList();
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 }
